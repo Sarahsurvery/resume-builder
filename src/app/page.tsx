c0895857bdd2b1fc229fd80fb4593dfea2118a3d
Sarahsurvery <sarahsurvery@gmail.com>
2024-11-26 22:54:25 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
 "use client";
-
-
 import { useState } from "react";
 import ResumeForm from "./components/ResumeForm";
-// import ResumePreview from "./components/ResumePreview";
-// import ResumeContent from "./components/ResumeContent";
-
-// import { data } from "motion/react-client";
-// import { form } from "motion/react-m";
+import generateResume from "./components/GenerateResume";
 
 export default function Home () {
 
-  // const ParentComponent = () =>{
   const [ FormData, setResumeData] = useState(null);
   const handleFormSubmit = (FormData:any) => {
     console.log('FormData received in parent:' ,FormData)
     setResumeData(FormData);
   };
-  return(
-    <div>
+    return(
+      <div>
       <h1>Resume Builder</h1>
       
         <ResumeForm onSubmit={handleFormSubmit} />
